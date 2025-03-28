fa8925fd46bb84148a935358f3bc91f51a8b54eb
Arthur Brider <199021082+ArthurB26@users.noreply.github.com>
2025-03-28 05:18:39 +0000

    feat: implement members area with username and password setup functionality

--- javascript/login.js
+++ javascript/login.js
@@ -9,14 +9,19 @@ function validatePassword() {
 
     try {
         const decodedPassword = atob(pass);
-        if (input === decodedPassword or input === document.cookie  && acceptedUsername.includes(username)) {
+        if (input === decodedPassword && acceptedUsername.includes(username)) {
             window.location.href = atob(URL);
+        if (document.cookie.includes(`username=${username}`) && document.cookie.includes(`password=${input}`)) {
+            window.location.href = atob(URL);z
         } else {
             errorMessage.textContent = "Invalid username or password. Please try again.";
         }
-    } catch (e) {
-        errorMessage.textContent = "An error occurred. Please try again.";
-    }
+        } else {
+            errorMessage.textContent = "Invalid username or password. Please try again.";
+        }
+        } catch (e) {
+            errorMessage.textContent = "An error occurred. Please try again.";
+        }
 }
 
 document.getElementById('loginButton').addEventListener('click', validatePassword);
---
+++ javascript/personal.js
@@ -0,0 +1,19 @@
+const username = document.cookie.split('; ').find(row => row.startsWith('username='))?.split('=')[1];
+const expiry = new Date();
+expiry.setDate(expiry.getDate() + 7);
+
+function setdetails() {
+    const password = document.getElementById('passwordInput').value;
+    const errorMessage = document.getElementById('errorMessage');
+    const username = document.getElementById('usernameInput').value;
+    document.cookie = `password=${password}; expires=${expiry.toUTCString()}; path=/`;
+    document.cookie = `username=${username}; path=/`;
+    document.getElementById('welcomeMessage').textContent = `Welcome ${username}`;
+    alert("Your new Username and Password were set succsessfully!")
+}
+
+document.getElementById('setuserpass').addEventListener('click', setdetails);
+
+if (username) {
+    document.getElementById('welcomeMessage').textContent = `Welcome ${username}`;
+}
