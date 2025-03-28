3d09c46c81d0fb0d6e9fc55921a613b76253712c
Arthur Brider <199021082+ArthurB26@users.noreply.github.com>
2025-03-28 03:26:08 +0000

    feat: enhance login functionality to include username validation

--- javascript/login.js
+++ javascript/login.js
@@ -1,23 +1,22 @@
-// Base64-encoded password ("test")
 const pass = "UmFtb25hNGV2ZXI=";
-const URL = "aHR0cHM6Ly9hcnRodXJiMjYuZ2l0aHViLmlvL3JhbW9uYWxvY2tlZG1lbWJlcnNwYWdlLw==";
+const URL = "bWVtYmVyc3BhZ2U=";
+const acceptedUsername = ["Leader", "Member"];
 
-// Function to validate the password
 function validatePassword() {
     const input = document.getElementById('passwordInput').value;
     const errorMessage = document.getElementById('errorMessage');
+    const username = document.getElementById('usernameInput').value;
 
     try {
         const decodedPassword = atob(pass);
-        if (input === decodedPassword) {
+        if (input === decodedPassword or input === document.cookie  && acceptedUsername.includes(username)) {
             window.location.href = atob(URL);
         } else {
-            errorMessage.textContent = "Invalid password. Please try again.";
+            errorMessage.textContent = "Invalid username or password. Please try again.";
         }
     } catch (e) {
         errorMessage.textContent = "An error occurred. Please try again.";
     }
 }
 
-// Attach the validatePassword function to the login button
 document.getElementById('loginButton').addEventListener('click', validatePassword);
